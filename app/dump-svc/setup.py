ff92da57ffaa2d8fd6c16578880628c5d96b1c50
Alejandro Gonzalez <alejandrojgt@gmail.com>
2021-01-21 23:14:00 +0900

    Make dummy package to use in dump-svc tests

---
+++ app/dump-svc/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="otokuna_dump_svc",
+    description="Minimal package to make service modules importable in tests.",
+    py_modules=[
+        "generate_base_path",
+        "dump_property_data"
+    ]
+)
