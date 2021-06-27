0edcdeccbae70e5b3614995683e91a2efb6cd11e
Alejandro Gonzalez <alejandrojgt@gmail.com>
2021-06-27 20:47:36 +0900

    Fix missing module in svc setup.py

--- svc/setup.py
+++ svc/setup.py
@@ -4,6 +4,7 @@ setup(
     name="otokuna_dump_svc",
     description="Minimal package to make service modules importable in tests.",
     py_modules=[
+        "build_search_url",
         "generate_base_path",
         "dump_property_data",
         "zip_property_data",
