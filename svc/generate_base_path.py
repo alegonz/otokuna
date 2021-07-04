a6ca99e935a9cc47ed4d4308ff8f4ce4d39363d2
Alejandro Gonzalez <alejandrojgt@gmail.com>
2021-07-04 17:05:46 +0900

    Refactor generate-base-path to accomodate for future user-requested case

--- svc/generate_base_path.py
+++ svc/generate_base_path.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from otokuna.dumping import now_isoformat
 
 
-def main(event, context):
+def main_daily(event, context):
     datetime_str = now_isoformat()
     base_path = Path("dumped_data") / "daily" / datetime_str / "東京都"
     event["base_path"] = str(base_path)
--- svc/tests/test_generate_base_path.py
+++ svc/tests/test_generate_base_path.py
@@ -4,8 +4,8 @@ import generate_base_path
 
 
 @freeze_time("2021-01-20T23:53:35+09:00")
-def test_main():
+def test_main_daily():
     event = {}
-    event_out = generate_base_path.main(event, None)
+    event_out = generate_base_path.main_daily(event, None)
     assert event_out is event
     assert event_out["base_path"] == "dumped_data/daily/2021-01-20T23:53:35+09:00/東京都"
