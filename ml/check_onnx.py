6faddbab838cad4e0e09c9a214004f7a190b1af3
Alejandro Gonzalez <alejandrojgt@gmail.com>
2021-07-10 00:03:36 +0900

    Don't hard code data and model paths in the ml scripts

--- ml/check_onnx.py
+++ ml/check_onnx.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Check that predictions are similar between the native format and ONNX"""
+import argparse
 import json
 
 import numpy as np
@@ -11,24 +12,36 @@ from otokuna.analysis import (
     add_address_coords, add_target_variable, df2Xy
 )
 
-# Read and preprocess data
-df = pd.read_pickle("data/2021-01-16T20:40:36+09:00/東京都.pickle")
-df = df.sample(frac=0.1, random_state=123)
-df = add_address_coords(df)
-df.dropna(inplace=True)
-df = add_target_variable(df)
-X, y = df2Xy(df)
 
-# Check
-model = CatBoostRegressor()
-model.load_model("models/regressor.cbm")
-y_pred_cbm = model.predict(X)
+def main(args):
+    # Read and preprocess data
+    df = pd.read_pickle(args.data_filename)
+    df = df.sample(frac=0.1, random_state=123)
+    df = add_address_coords(df)
+    df.dropna(inplace=True)
+    df = add_target_variable(df)
+    X, y = df2Xy(df)
 
-sess = InferenceSession("models/regressor.onnx")
-onnx_out = sess.run(["predictions"], {"features": X.values.astype(np.float32)})
-y_pred_onnx = onnx_out[0].squeeze()
+    # Check
+    model = CatBoostRegressor()
+    model.load_model(args.model_cbm_filename)
+    y_pred_cbm = model.predict(X)
 
-np.testing.assert_allclose(y_pred_onnx, y_pred_cbm, rtol=1e-5)
-max_ape = np.max(np.abs((y_pred_cbm - y_pred_onnx) / y_pred_cbm))
-with open("check_onnx.json", "w") as file:
-    json.dump({"maxAPE_cbm_onnx": max_ape}, file)
+    sess = InferenceSession(args.model_onnx_filename)
+    onnx_out = sess.run(["predictions"], {"features": X.values.astype(np.float32)})
+    y_pred_onnx = onnx_out[0].squeeze()
+
+    np.testing.assert_allclose(y_pred_onnx, y_pred_cbm, rtol=1e-5)
+    max_ape = np.max(np.abs((y_pred_cbm - y_pred_onnx) / y_pred_cbm))
+    with open(args.out_filename, "w") as file:
+        json.dump({"maxAPE_cbm_onnx": max_ape}, file)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Check onnx model produces an output close "
+                                                 "to that of its version in cbm format")
+    parser.add_argument("data_filename", help="Input data filename (pickle format)")
+    parser.add_argument("model_onnx_filename", help="Model filename in onnx format")
+    parser.add_argument("model_cbm_filename", help="Model filename in cbm format")
+    parser.add_argument("--out-filename", default="check_onnx.json", help="Output filename")
+    main(parser.parse_args())
--- ml/train.py
+++ ml/train.py
@@ -3,6 +3,7 @@
 Train regression model to predict median average price of properties.
 TODO: Move parameters to a params.yaml file.
 """
+import argparse
 import json
 from collections import defaultdict
 
@@ -22,50 +23,59 @@ def mae(y_true, y_pred):
     return np.mean(np.abs(y_true - y_pred), axis=0)
 
 
-# Read and preprocess data
-df = pd.read_pickle("data/2021-01-16T20:40:36+09:00/東京都.pickle")
-df = add_address_coords(df)
-df = add_target_variable(df)
-df = clean_df(df)
+def main(args):
+    # Read and preprocess data
+    df = pd.read_pickle(args.data_filename)
+    df = add_address_coords(df)
+    df = add_target_variable(df)
+    df = clean_df(df)
 
-# Split datasets
-(df_train, df_val, df_test), *_ = train_val_test_split(
-    [df], val_ratio=0.1875, test_ratio=0.25, seed=123
-)
-X_train, y_train = df2Xy(df_train)
-X_val, y_val = df2Xy(df_val)
-X_test, y_test = df2Xy(df_test)
+    # Split datasets
+    (df_train, df_val, df_test), *_ = train_val_test_split(
+        [df], val_ratio=0.1875, test_ratio=0.25, seed=123
+    )
+    X_train, y_train = df2Xy(df_train)
+    X_val, y_val = df2Xy(df_val)
+    X_test, y_test = df2Xy(df_test)
 
-# Train model
-model = CatBoostRegressor(
-    learning_rate=1e-2,
-    iterations=10000,
-    loss_function="MAE",
-    random_seed=456
-)
+    # Train model
+    model = CatBoostRegressor(
+        learning_rate=1e-2,
+        iterations=10000,
+        loss_function="MAE",
+        random_seed=456
+    )
 
-_ = model.fit(
-    X_train, y_train,
-    eval_set=(X_val, y_val),
-    use_best_model=True,
-    early_stopping_rounds=10
-)
+    _ = model.fit(
+        X_train, y_train,
+        eval_set=(X_val, y_val),
+        use_best_model=True,
+        early_stopping_rounds=10
+    )
 
-# Evaluate model
-datasets = {"train": (X_train, y_train), "val": (X_val, y_val), "test": (X_test, y_test)}
-metrics = defaultdict(dict)
-for set_name, (X, y) in datasets.items():
-    y_pred = model.predict(X)
-    metrics[set_name]["MAE"] = mae(y, y_pred)
+    # Evaluate model
+    datasets = {"train": (X_train, y_train), "val": (X_val, y_val), "test": (X_test, y_test)}
+    metrics = defaultdict(dict)
+    for set_name, (X, y) in datasets.items():
+        y_pred = model.predict(X)
+        metrics[set_name]["MAE"] = mae(y, y_pred)
 
-with open("metrics.json", "w") as file:
-    json.dump(metrics, file)
+    with open(args.metrics_filename, "w") as file:
+        json.dump(metrics, file)
 
-# Save model
-# We drop the model_guid and train_finish_time metadata to ensure that the
-# produced files are reproducible and result in the same hash always, which
-# is necessary to ensure correct tracking with DVC.
-for key in ("model_guid", "train_finish_time"):
-    del model.get_metadata()[key]
-for format_ in ("cbm", "onnx"):
-    model.save_model(f"models/regressor.{format_}", format=format_)
+    # Save model
+    # We drop the model_guid and train_finish_time metadata to ensure that the
+    # produced files are reproducible and result in the same hash always, which
+    # is necessary to ensure correct tracking with DVC.
+    for key in ("model_guid", "train_finish_time"):
+        del model.get_metadata()[key]
+    for format_ in ("cbm", "onnx"):
+        model.save_model(f"{args.model_filename}.{format_}", format=format_)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Train model")
+    parser.add_argument("data_filename", help="Input data filename (pickle format)")
+    parser.add_argument("model_filename", help="Output model filename (extension will be added automatically)")
+    parser.add_argument("--metrics-filename", default="metrics.json", help="Output metrics filename")
+    main(parser.parse_args())
